7b9a716cd52fc92a4245bd4dbcfbe4651e05c10d
pkavi <pranavk2014@gmail.com>
2016-09-29 15:01:07 -0500

    Added DFS tests

---
+++ assignment3/DFSSpecificTests.java
@@ -0,0 +1,93 @@
+package assignment3;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class DFSSpecificTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void DFSLadder1() {
+		String start = "SMART";
+		String end = "MONEY";
+		Main.initialize();
+		long startTime=System.currentTimeMillis();
+		ArrayList<String> ladder = Main.getWordLadderDFS(start, end);
+		long diff=Math.abs(startTime-System.currentTimeMillis());
+		if (diff>30000){
+			fail("Too slow");
+		}
+	
+	}
+	
+	@Test
+	public void emptyLadderDFS1() {
+		String start = "SMART";
+		String end = "ALOOF";
+		Main.initialize();
+		long startTime=System.currentTimeMillis();
+		ArrayList<String> ladder = Main.getWordLadderDFS(start, end);
+		long diff=Math.abs(startTime-System.currentTimeMillis());
+		if (diff>30000){
+			fail("Too slow");
+		}
+	
+	}
+	
+	@Test
+	public void emptyLadderDFS2() {
+		String start = "JAZZY";
+		String end = "MONEY";
+		Main.initialize();
+		long startTime=System.currentTimeMillis();
+		ArrayList<String> ladder = Main.getWordLadderDFS(start, end);
+		long diff=Math.abs(startTime-System.currentTimeMillis());
+		if (diff>30000){
+			fail("Too slow");
+		}
+		if (ladder.size()!= 0){
+			fail("Not correct size");
+		}
+	}
+	
+	@Test
+	public void zeroRungDFS() {
+		String start = "SMART";
+		String end = "START";
+		Main.initialize();
+		long startTime=System.currentTimeMillis();
+		ArrayList<String> ladder = Main.getWordLadderDFS(start, end);
+		long diff=Math.abs(startTime-System.currentTimeMillis());
+		if (diff>30000){
+			fail("Too slow");
+		}
+
+	}
+	
+	@Test
+	public void DFSLadder2() {
+		String start = "SMART";
+		String end = "BRAIN";
+		Main.initialize();
+		long startTime=System.currentTimeMillis();
+		ArrayList<String> ladder = Main.getWordLadderDFS(start, end);
+		long diff=Math.abs(startTime-System.currentTimeMillis());
+		if (diff>30000){
+			fail("Too slow");
+		}
+
+	} 
+	@Test
+	public void DFSLadder3(){
+		
+		
+		
+		
+	}
+}
